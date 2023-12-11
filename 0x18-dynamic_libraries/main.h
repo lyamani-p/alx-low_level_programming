2a0a4cb6e16134561e018c28ee3f00372f583add
lyamani-p <lyamani.abderrahmane@gmail.com>
2023-12-11 12:20:21 -0800

    main.h

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,27 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int _islower(int c);
+int _abs(int n);
+int _isalpha(int c);
+char *_strcpy(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char 
+int _atoi(char *s);
+char *_strcat(char *
+int _strlen(char *s);
+int _isupper(int c);
+int _isdigit(int c);
+void _puts(char *s);
+dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+*dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif
